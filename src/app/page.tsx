00d867ce4be088a5c6b30efd17ed477b614d7b8d
VitorLostadaC <vitorlostada@hotmail.com>
2024-07-21 15:30:07 -0300

    feat: change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <img src={logo.src} alt="Logo" width={300} height={300} />
       <h1 className="text-8xl font-bold mt-8 mb-4 text-red-500">
-        Se inscreva no canal
+        Ative o sininho
       </h1>
       <p className="text-xl text-gray-700">Vitor Cunha Code</p>
     </main>
